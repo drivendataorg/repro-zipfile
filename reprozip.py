05405707c778b0abc51cab0751e63f976467da24
Jay Qi <jayqi@users.noreply.github.com>
2023-08-11 01:08:34 -0400

    Copy code instead of using super methods

--- reprozip.py
+++ reprozip.py
@@ -1,24 +1,67 @@
-from pathlib import Path
-from zipfile import ZipFile, ZipInfo
+from copy import copy
+import shutil
+import sys
+from zipfile import ZIP_LZMA, ZipFile, ZipInfo
+
+try:
+    from zipfile import _MASK_COMPRESS_OPTION_1
+except ImportError:
+    _MASK_COMPRESS_OPTION_1 = 0x02
 
 __version__ = "0.1"
 
 
 class ReproducibleZipFile(ZipFile):
+    """Open a ZIP file, where file can be a path to a file (a string), a file-like object or a
+    path-like object.
+
+    This is a replacement for the Python standard library zipfile.ZipFile that
+    overwrites file-modified timestamps in write mode in order to create a reproducible ZIP
+    archive. For documentation on use, see the Python documentation for zipfile:
+    https://docs.python.org/3/library/zipfile.html
+    """
+
+    # Modified from Python 3.11
+    # https://github.com/python/cpython/blob/202efe1a3bcd499f3bf17bd953c6d36d47747e78/Lib/zipfile.py#L1763-L1794
     def write(self, filename, arcname=None, compress_type=None, compresslevel=None):
-        if arcname is None:
-            arcname = filename
+        """Put the bytes from filename into the archive under the name arcname."""
 
-        self.writestr(
-            zinfo_or_arcname=str(arcname),
-            data=Path(filename).read_bytes(),
-            compress_type=compress_type,
-            compresslevel=compresslevel,
-        )
+        if not self.fp:
+            raise ValueError("Attempt to write to ZIP archive that was already closed")
+        if self._writing:
+            raise ValueError("Can't write to ZIP archive while an open writing handle exists")
 
+        zinfo = ZipInfo.from_file(filename, arcname, strict_timestamps=self._strict_timestamps)
+        zinfo.date_time = (1980, 1, 1, 0, 0, 0)  # ADDED
+
+        if zinfo.is_dir():
+            zinfo.compress_size = 0
+            zinfo.CRC = 0
+            self.mkdir(zinfo)
+        else:
+            if compress_type is not None:
+                zinfo.compress_type = compress_type
+            else:
+                zinfo.compress_type = self.compression
+
+            if compresslevel is not None:
+                zinfo._compresslevel = compresslevel
+            else:
+                zinfo._compresslevel = self.compresslevel
+
+            with open(filename, "rb") as src, self.open(zinfo, "w") as dest:
+                shutil.copyfileobj(src, dest, 1024 * 8)
+
+    # Modified from Python 3.11
+    # https://github.com/python/cpython/blob/202efe1a3bcd499f3bf17bd953c6d36d47747e78/Lib/zipfile.py#L1796-L1835
     def writestr(self, zinfo_or_arcname, data, compress_type=None, compresslevel=None):
+        """Write a file into the archive.  The contents is 'data', which may be either a 'str' or
+        a 'bytes' instance; if it is a 'str', it is encoded as UTF-8 first. 'zinfo_or_arcname' is
+        either a ZipInfo instance or the name of the file in the archive."""
+        if isinstance(data, str):
+            data = data.encode("utf-8")
         if not isinstance(zinfo_or_arcname, ZipInfo):
-            zinfo = ZipInfo(filename=zinfo_or_arcname, date_time=(1980, 1, 1, 0, 0, 0))
+            zinfo = ZipInfo(filename=zinfo_or_arcname, date_time=(1980, 1, 1, 0, 0, 0))  # CHANGED
             zinfo.compress_type = self.compression
             zinfo._compresslevel = self.compresslevel
             if zinfo.filename.endswith("/"):
@@ -27,10 +70,59 @@ class ReproducibleZipFile(ZipFile):
             else:
                 zinfo.external_attr = 0o600 << 16  # ?rw-------
         else:
-            zinfo = ZipInfo(filename=zinfo_or_arcname.filename, date_time=(1980, 1, 1, 0, 0, 0))
+            zinfo = copy(zinfo_or_arcname)  # CHANGED
+            zinfo.date_time = (1980, 1, 1, 0, 0, 0)  # ADDED
 
-        super().writestr(
-            zinfo, data=data, compress_type=compress_type, compresslevel=compresslevel
-        )
+        if not self.fp:
+            raise ValueError("Attempt to write to ZIP archive that was already closed")
+        if self._writing:
+            raise ValueError("Can't write to ZIP archive while an open writing handle exists.")
 
-        self.filelist = sorted(self.filelist, key=lambda zinfo: zinfo.filename)
+        if compress_type is not None:
+            zinfo.compress_type = compress_type
+
+        if compresslevel is not None:
+            zinfo._compresslevel = compresslevel
+
+        zinfo.file_size = len(data)  # Uncompressed size
+        with self._lock:
+            with self.open(zinfo, mode="w") as dest:
+                dest.write(data)
+
+    if sys.version_info < (3, 11):
+        # Copied from Python 3.11
+        # https://github.com/python/cpython/blob/202efe1a3bcd499f3bf17bd953c6d36d47747e78/Lib/zipfile.py#L1837-L1870
+        def mkdir(self, zinfo_or_directory_name, mode=511):
+            """Creates a directory inside the zip archive."""
+            if isinstance(zinfo_or_directory_name, ZipInfo):
+                zinfo = zinfo_or_directory_name
+                if not zinfo.is_dir():
+                    raise ValueError("The given ZipInfo does not describe a directory")
+            elif isinstance(zinfo_or_directory_name, str):
+                directory_name = zinfo_or_directory_name
+                if not directory_name.endswith("/"):
+                    directory_name += "/"
+                zinfo = ZipInfo(directory_name)
+                zinfo.compress_size = 0
+                zinfo.CRC = 0
+                zinfo.external_attr = ((0o40000 | mode) & 0xFFFF) << 16
+                zinfo.file_size = 0
+                zinfo.external_attr |= 0x10
+            else:
+                raise TypeError("Expected type str or ZipInfo")
+
+            with self._lock:
+                if self._seekable:
+                    self.fp.seek(self.start_dir)
+                zinfo.header_offset = self.fp.tell()  # Start of header bytes
+                if zinfo.compress_type == ZIP_LZMA:
+                    # Compressed data includes an end-of-stream (EOS) marker
+                    zinfo.flag_bits |= _MASK_COMPRESS_OPTION_1
+
+                self._writecheck(zinfo)
+                self._didModify = True
+
+                self.filelist.append(zinfo)
+                self.NameToInfo[zinfo.filename] = zinfo
+                self.fp.write(zinfo.FileHeader(False))
+                self.start_dir = self.fp.tell()
--- tests.py
+++ tests.py
@@ -1,7 +1,13 @@
 import hashlib
+import os
 from pathlib import Path
+import shutil
 from time import sleep
 import uuid
+from zipfile import ZipFile
+
+import pytest
+from pytest_cases import fixture_union
 
 from reprozip import ReproducibleZipFile
 
@@ -16,32 +22,162 @@ def hash_file(path: Path):
     return hashlib.md5(path.read_bytes()).hexdigest()
 
 
-def test_write_same_file_different_mtime(tmp_path):
+@pytest.fixture
+def abs_path(tmp_path):
+    """Fixture that returns a temporary directory as an absolute Path object."""
+    return tmp_path
+
+
+@pytest.fixture
+def rel_path(tmp_path):
+    """Fixture that sets a temporary directory as the current working directory and returns a
+    relative path to it."""
+    orig_wd = Path.cwd()
+    os.chdir(tmp_path)
+    yield Path()
+    os.chdir(orig_wd)
+
+
+base_path = fixture_union("base_path", ["rel_path", "abs_path"])
+
+
+def test_write(base_path):
+    """Test that write adds files to the archive in the expected way, i.e., we didn't break the
+    basic functionality."""
+    data_file = base_path / "data.txt"
+    data_file.write_text(data_factory())
+    data_dir = base_path / "dir"
+    data_dir.mkdir()
+    for i in range(3):
+        (data_dir / f"{i}.txt").write_text(data_factory())
+
+    # Create and extract ReproducibleZipFile
+    reprozip_file = base_path / "reprozip_file.zip"
+    with ReproducibleZipFile(reprozip_file, "w") as zp:
+        zp.write(data_file)
+        for root, dirs, files in os.walk(data_dir):
+            for d in dirs:
+                zp.write(Path(root) / d)
+            for f in files:
+                zp.write(Path(root) / f)
+    reprozip_outdir = base_path / "reprozip_out"
+    reprozip_outdir.mkdir()
+    with ReproducibleZipFile(reprozip_file, "r") as zp:
+        zp.extractall(reprozip_outdir)
+
+    # Create and extract regular ZipFile for comparison
+    zip_file = base_path / "zip_file.zip"
+    with ZipFile(zip_file, "w") as zp:
+        zp.write(data_file)
+        for root, dirs, files in os.walk(data_dir):
+            for d in dirs:
+                zp.write(Path(root) / d)
+            for f in files:
+                zp.write(Path(root) / f)
+    zip_outdir = base_path / "zip_out"
+    zip_outdir.mkdir()
+    with ZipFile(zip_file, "r") as zp:
+        zp.extractall(zip_outdir)
+
+    reprozip_extracted = sorted(reprozip_outdir.glob("**/*"))
+    zip_extracted = sorted(zip_outdir.glob("**/*"))
+    assert len(reprozip_extracted) == len(zip_extracted)
+    for reprozip_member, zip_member in zip(reprozip_extracted, zip_extracted):
+        assert reprozip_member.relative_to(reprozip_outdir) == zip_member.relative_to(zip_outdir)
+        if reprozip_member.is_file():
+            assert zip_member.is_file()
+            assert reprozip_member.read_text() == zip_member.read_text()
+
+
+def test_writestr(tmp_path):
+    """Test that writestr adds files to the archive in the expected way, i.e., we didn't break the
+    basic functionality."""
     data = data_factory()
-    data_file = tmp_path / "data.txt"
+
+    with ReproducibleZipFile(tmp_path / "reprozip.zip", "w") as zp:
+        zp.writestr("data.txt", data=data)
+
+    extract_dir = tmp_path / "extract"
+    extract_dir.mkdir()
+
+    with ZipFile(tmp_path / "reprozip.zip", "r") as zp:
+        zp.extractall(extract_dir)
+
+    assert sorted(extract_dir.glob("**/*")) == [extract_dir / "data.txt"]
+    assert (extract_dir / "data.txt").read_text() == data
+
+
+def test_write_same_file_different_mtime(base_path):
+    """Test that writing the same file with different mtime produces the same hash."""
+    data = data_factory()
+    data_file = base_path / "data.txt"
 
     data_file.write_text(data)
-    with ReproducibleZipFile(tmp_path / "zip1.zip", "w") as zp:
+    with ReproducibleZipFile(base_path / "zip1.zip", "w") as zp:
         zp.write(data_file)
 
     sleep(2)
 
     data_file.write_text(data)
-    with ReproducibleZipFile(tmp_path / "zip2.zip", "w") as zp:
+    with ReproducibleZipFile(base_path / "zip2.zip", "w") as zp:
         zp.write(data_file)
 
-    assert hash_file(tmp_path / "zip1.zip") == hash_file(tmp_path / "zip2.zip")
+    assert hash_file(base_path / "zip1.zip") == hash_file(base_path / "zip2.zip")
 
 
-def test_writestr_same_data_different_mtime(tmp_path):
+def test_write_same_file_different_mtime_string_input(rel_path):
+    """Test that writing the same file with different mtime produces the same hash, using string
+    inputs instead of Path."""
+    data = data_factory()
+    data_file = rel_path / "data.txt"
+
+    data_file.write_text(data)
+    with ReproducibleZipFile(rel_path / "zip1.zip", "w") as zp:
+        zp.write("data.txt")
+
+    sleep(2)
+
+    data_file.write_text(data)
+    with ReproducibleZipFile(rel_path / "zip2.zip", "w") as zp:
+        zp.write("data.txt")
+
+    assert hash_file(rel_path / "zip1.zip") == hash_file(rel_path / "zip2.zip")
+
+
+def test_write_same_directory_different_mtime(base_path):
+    data_list = [data_factory() for _ in range(3)]
+    data_dir = base_path / "dir"
+
+    data_dir.mkdir()
+    for data in data_list:
+        (data_dir / f"{data}.txt").write_text(data)
+    with ReproducibleZipFile(base_path / "zip1.zip", "w") as zp:
+        zp.write(data_dir)
+        print(zp.infolist())
+
+    sleep(2)
+
+    shutil.rmtree(data_dir)
+    data_dir.mkdir()
+    for data in data_list:
+        (data_dir / f"{data}.txt").write_text(data)
+    with ReproducibleZipFile(base_path / "zip2.zip", "w") as zp:
+        zp.write(data_dir)
+        print(zp.infolist())
+
+    assert hash_file(base_path / "zip1.zip") == hash_file(base_path / "zip2.zip")
+
+
+def test_writestr_same_data_different_mtime(base_path):
+    """Test that using writestr with the same data at different times produces the same hash."""
     data = data_factory()
 
-    with ReproducibleZipFile(tmp_path / "zip1.zip", "w") as zp:
+    with ReproducibleZipFile(base_path / "zip1.zip", "w") as zp:
         zp.writestr("data.txt", data=data)
 
     sleep(2)
 
-    with ReproducibleZipFile(tmp_path / "zip2.zip", "w") as zp:
+    with ReproducibleZipFile(base_path / "zip2.zip", "w") as zp:
         zp.writestr("data.txt", data=data)
 
-    assert hash_file(tmp_path / "zip1.zip") == hash_file(tmp_path / "zip2.zip")
+    assert hash_file(base_path / "zip1.zip") == hash_file(base_path / "zip2.zip")
